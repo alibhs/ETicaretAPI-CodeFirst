cb8ec655fca0256e9ddba83aecaec033e346c237
alibhs <alibahsi772@gmail.com>
2023-12-22 23:33:31 +0300

    SaveChangeAsync Interceptor

--- Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
+++ Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -9,6 +9,7 @@ namespace ETicaretAPI.Domain.Entities.Common
     public class BaseEntity
     {
         public Guid Id { get; set; }
-        public DateTime CreatedTime { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
--- Infrastucture/ETicaretAPI.Persistance/Contexts/ETicaretAPIDbContext.cs
+++ Infrastucture/ETicaretAPI.Persistance/Contexts/ETicaretAPIDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -15,5 +16,22 @@ namespace ETicaretAPI.Persistance.Contexts
             public DbSet<Product> Products { get; set; }
             public DbSet<Order> Orders { get; set; }
             public DbSet<Customer> Customers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            //interceptor ekliyoruz
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow,
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
