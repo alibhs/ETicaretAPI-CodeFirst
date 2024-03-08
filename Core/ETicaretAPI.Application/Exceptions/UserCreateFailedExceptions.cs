a31bc36194441dc44a08691f77fccef1dc5ee3fd
alibhs <alibahsi772@gmail.com>
2024-03-08 16:30:44 +0300

    Identity Mekanizmasının Kurulumu ve Kullanıcı Ekleme

---
+++ Core/ETicaretAPI.Application/Exceptions/UserCreateFailedExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Exceptions
+{
+    public class UserCreateFailedExceptions : Exception
+    {
+        public UserCreateFailedExceptions() :base("Kullanıcı oluşturulurken beklenmeyen bir hata ile karşılaşıldı")
+        {
+        }
+
+        public UserCreateFailedExceptions(string? message) : base(message)
+        {
+        }
+
+        public UserCreateFailedExceptions(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- Infrastucture/ETicaretAPI.Persistance/ServiceRegistration.cs
+++ Infrastucture/ETicaretAPI.Persistance/ServiceRegistration.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using ETicaretAPI.Application.Repositories;
 using ETicaretAPI.Persistance.Repositories;
+using ETicaretAPI.Domain.Entities.Identity;
 
 namespace ETicaretAPI.Persistance
 {
@@ -15,19 +16,29 @@ namespace ETicaretAPI.Persistance
     {
         public static void AddPersistanceServices(this IServiceCollection services)
         {
+
             services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
-            services.AddScoped<ICustomerReadRepository,CustomerReadRepository>();
-            services.AddScoped<ICustomerWriteRepository,CustomerWriteRepository>();
-            services.AddScoped<IOrderReadRepository,OrderReadRepository>();
-            services.AddScoped<IOrderWriteRepository,OrderWriteRepository>();
+            services.AddIdentity<AppUser, AppRole>(
+                options =>
+                {
+                    options.Password.RequiredLength = 3;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                }).AddEntityFrameworkStores<ETicaretAPIDbContext>();
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
             services.AddScoped<IProductReadRepository, ProductReadRepository>();
-            services.AddScoped<IProductWriteRepository,ProductWriteRepository>();
-            services.AddScoped<IFileReadRepository,FileReadRepository>();
-            services.AddScoped<IFileWriteRepository,FileWriteRepository>();
-            services.AddScoped<IProductImageReadRepository,ProductImageFileReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IFileReadRepository, FileReadRepository>();
+            services.AddScoped<IFileWriteRepository, FileWriteRepository>();
+            services.AddScoped<IProductImageReadRepository, ProductImageFileReadRepository>();
             services.AddScoped<IProductImageWriteRepository, ProductImageFileWriteRepository>();
-            services.AddScoped<IInvoiceFileReadRepository,InvoiceFileReadRepository>();
-            services.AddScoped<IInvoiceFileWriteRepository,InvoiceFileWriteRepository>();
+            services.AddScoped<IInvoiceFileReadRepository, InvoiceFileReadRepository>();
+            services.AddScoped<IInvoiceFileWriteRepository, InvoiceFileWriteRepository>();
         }
     }
 }
